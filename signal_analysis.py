d95bcb036328cadafe025690a47e17a59fe218c4
Remy-Lyscar <remy.lyscar@orange.fr>
2024-06-26 18:52:12 +0100

    new nice plots

--- signal_analysis.py
+++ signal_analysis.py
@@ -7,10 +7,8 @@ Created on Tue Jun 25 10:50:40 2024
 """
 
 import time 
-from pyrpl.async_utils import sleep
 import numpy as np
 import matplotlib.pyplot as plt 
-from pyrpl import Pyrpl
 import pandas as pd 
 import pickle 
 import os 
@@ -20,9 +18,13 @@ import allantools
 from scipy.optimize import curve_fit
 import scienceplots 
 from numpy.fft import fft
+# import pathlib
+# import matplotlib as mpl
+
+# pathlib.Path.rmdir( mpl.get_cachedir())
 
 
-plt.style.use('science')
+# plt.style.use('science')
 
 
 
@@ -36,8 +38,8 @@ if not os.path.exists(figures_dir):
     
 
 
-free = pickle.load(open('free_running_signal.pkl', "rb"))
-locked = pickle.load(open('locked_laser.pkl', "rb"))
+free = pickle.load(open('free_running_signal_mer_26_juin.pkl', "rb"))
+locked = pickle.load(open('locked_laser_mer_26_juin.pkl', "rb"))
 
 
 times_free = free['times']
@@ -46,10 +48,6 @@ signal_free = free['signal']
 signal_locked = locked['signal']
 
 
-tmp = [(times_free[i], signal_free[i]) for i in range(len(times_free))]
-tmp = [x for x in tmp if x[1] < 0.125]
-times_free = [tmp[i][0] for i in range(len(tmp))]
-signal_free = [tmp[i][1] for i in range(len(tmp))]
 
 times_free = np.array(times_free)
 signal_free = np.array(signal_free)
@@ -77,11 +75,11 @@ signal_free = np.array(signal_free)
 
 
 
-# print(f'{prop}%')
-# print(f'{prop2}%')
+# print(y_up - y_down)
+# print(y_up2 - y_down2)
 
 # fig = plt.figure()
-# plt.plot(times_free, signal_free, 'b.', zorder = 0)
+# plt.plot(times_free, signal_free, color = 'cornflowerblue', marker = '.', linestyle = '', zorder = 0)
 
 # xmax = times_free[np.argmax(signal_free)]
 # ymax = signal_free.max()
@@ -94,15 +92,17 @@ signal_free = np.array(signal_free)
 # ax.set_ylim(y_bounds[0], y_bounds[1])
 # plt.hlines(y_up2, x_bounds[0] ,x_bounds[1], colors ='orange', linestyles='dashed')
 # plt.hlines(y_down2, x_bounds[0] ,x_bounds[1], colors='orange', linestyles='dashed')
-# plt.hlines(y_up, x_bounds[0] ,x_bounds[1], colors='red', linestyles='dashed', zorder=1, label = f'std = {d:.3f}')
+# plt.hlines(y_up, x_bounds[0] ,x_bounds[1], colors='red', linestyles='dashed', zorder=1, label = f'std = {d:.4f}')
 # plt.hlines(y_down, x_bounds[0] ,x_bounds[1], colors='red', linestyles='dashed', zorder=1)
-# plt.hlines(m, x_bounds[0] ,x_bounds[1], colors='black', linestyles='dashed', zorder=1, label = f'mean = {m:.3f}') 
+# plt.hlines(m, x_bounds[0] ,x_bounds[1], colors='black', linestyles='dashed', zorder=1, label = f'mean = {m:.4f}') 
 
 
-# plt.legend() 
+# plt.legend(fontsize = 20) 
 # plt.xlabel('times (seconds)')
 # plt.ylabel('voltage (V)')
 # plt.title('Photodiode Voltage when the laser is free')
+# plt.rc('axes', titlesize=20)
+# plt.rc('axes', labelsize = 20) 
 # plot_file = os.path.join(figures_dir, "free_laser.png")
 # plt.show()
 # # plt.savefig(plot_file)
@@ -115,58 +115,60 @@ signal_free = np.array(signal_free)
 ### Power-locked laser ###
 
 
-m = np.mean(signal_locked)
-d = np.std(signal_locked, ddof = 1)
+# m = np.mean(signal_locked)
+# d = np.std(signal_locked, ddof = 1)
 
-y_up = m + d
-y_down = m - d
-
-
-y_up2 = m + 2*d
-y_down2 = m - 2*d
+# y_up = m + d
+# y_down = m - d
 
 
-# Computation of the percentage of the points which are within the red band and in the orange band 
-in_band_free = [s for s in signal_free if s >= y_down and s <= y_up]
-prop = (len(in_band_free)/len(signal_free)) * 100
-
-in_band_free2 = [s for s in signal_free if s >= y_down2 and s <= y_up2]
-prop2 = (len(in_band_free2)/len(signal_free)) * 100
+# y_up2 = m + 2*d
+# y_down2 = m - 2*d
 
 
+# # Computation of the percentage of the points which are within the red band and in the orange band 
+# in_band_locked = [s for s in signal_locked if s >= y_down and s <= y_up]
+# prop = (len(in_band_locked)/len(signal_locked)) * 100
 
-print(f'{prop}%')
-print(f'{prop2}%')
-
-
-fig = plt.figure()
-plt.plot(times_locked, signal_locked, 'b.', zorder = 0)
-
-xmax = times_locked[np.argmax(signal_locked)]
-ymax = signal_locked.max()
-xmin = times_locked[np.argmin(signal_locked)]
-ymin = signal_locked.min()
-ax=plt.gca()
-x_bounds = ax.get_xbound()
-y_bounds = ax.get_ybound()
-ax.set_xlim(x_bounds[0], x_bounds[1])
-ax.set_ylim(y_bounds[0], y_bounds[1])
-plt.hlines(y_up2, x_bounds[0] ,x_bounds[1], colors ='orange', linestyles='dashed')
-plt.hlines(y_down2, x_bounds[0] ,x_bounds[1], colors='orange', linestyles='dashed')
-plt.hlines(y_up, x_bounds[0] ,x_bounds[1], colors='red', linestyles='dashed', zorder=1, label = f'std = {d:.3f}')
-plt.hlines(y_down, x_bounds[0] ,x_bounds[1], colors='red', linestyles='dashed', zorder=1)
-plt.hlines(m, x_bounds[0] ,x_bounds[1], colors='black', linestyles='dashed', zorder=1, label = f'mean = {m:.3f}') 
+# in_band_locked2 = [s for s in signal_locked if s >= y_down2 and s <= y_up2]
+# prop2 = (len(in_band_locked2)/len(signal_locked)) * 100
 
 
-plt.legend() 
-plt.xlabel('times (seconds)')
-plt.ylabel('voltage (V)')
-plt.title('Photodiode Voltage when the laser is locked at 0.09V')
-plot_file = os.path.join(figures_dir, "locked_laser.png")
-plt.show()
-# plt.savefig(plot_file)
 
-print(m, d)
+# print(y_up - y_down)
+# print(y_up2 - y_down2)
+
+
+# fig = plt.figure()
+# plt.plot(times_locked, signal_locked, color = 'cornflowerblue', marker = '.', linestyle = '', zorder = 0)
+
+# xmax = times_locked[np.argmax(signal_locked)]
+# ymax = signal_locked.max()
+# xmin = times_locked[np.argmin(signal_locked)]
+# ymin = signal_locked.min()
+# ax=plt.gca()
+# x_bounds = ax.get_xbound()
+# y_bounds = ax.get_ybound()
+# ax.set_xlim(x_bounds[0], x_bounds[1])
+# ax.set_ylim(y_bounds[0], y_bounds[1])
+# plt.hlines(y_up2, x_bounds[0] ,x_bounds[1], colors ='orange', linestyles='dashed')
+# plt.hlines(y_down2, x_bounds[0] ,x_bounds[1], colors='orange', linestyles='dashed')
+# plt.hlines(y_up, x_bounds[0] ,x_bounds[1], colors='red', linestyles='dashed', zorder=1, label = f'std = {d:.4f}')
+# plt.hlines(y_down, x_bounds[0] ,x_bounds[1], colors='red', linestyles='dashed', zorder=1)
+# plt.hlines(m, x_bounds[0] ,x_bounds[1], colors='black', linestyles='dashed', zorder=1, label = f'mean = {m:.4f}') 
+
+
+# plt.legend(fontsize=20) 
+# plt.xlabel('times (seconds)')
+# plt.ylabel('voltage (V)')
+# plt.title('Photodiode Voltage for a lock at 0.08V')
+# plt.rc('axes', titlesize=20)
+# plt.rc('axes', labelsize = 20) 
+# plot_file = os.path.join(figures_dir, "locked_laser.png")
+# plt.show()
+# # plt.savefig(plot_file)
+
+# print(m, d)
 
 
 # Comparison with both plots on the same figure may not be apt since there 
@@ -190,77 +192,46 @@ print(m, d)
 
 
 
-# def fit_function(x, a, b): 
-#     return a*x + b
+def fit_function(tau, a, b): 
+    return a*tau**b
 
 
-# ### Free running signal ### 
+### Free running signal ### 
 
-# t = np.logspace(0,2, 50)  # tau values from 1 to 100 -> better to have more date though time 
-# t2, ad2, aderr2, adn2 = allantools.oadev(signal_free, rate = 0.25, data_type = 'phase', taus = t)
-# # Rq: je mets les deux en type 'phase' car c'est le seul qui marche pour le signal_locked -> Pourquoi ??
-
-
-# fig = plt.figure()
-# plt.plot(np.log(t2), np.log(ad2), label = 'free running laser')
-# plt.xlabel('Taus (seconds)')
-# plt.ylabel('Overlapping Allan deviation')
-# plt.grid()
-# popt, pcov = curve_fit(fit_function, np.log(t2), np.log(ad2))
-# a2 = popt[0]
-# b2 = popt[1]
-# plt.plot(np.log(t2), fit_function(np.log(t2), *popt), color='r', linestyle = 'dashed', label = f'linear fit: y = {a2:.3f}x + {b2:.3f}')
-# perr2 = np.sqrt(np.diag(pcov))
-# plt.legend()
-# plt.show()
+t = np.logspace(0,3.55, 50)
+t2, ad2, aderr2, adn2 = allantools.oadev(signal_free, rate = 0.25, data_type = 'phase', taus = t)
+# Rq: je mets les deux en type 'phase' car c'est le seul qui marche pour le signal_locked -> Pourquoi ??
 
 
-# ### Power-locked laser ### 
+fig = plt.figure()
+plt.loglog(t2, ad2, label = 'free running laser', color = 'b', linestyle = '', marker = '.')
+plt.xlabel('Taus (seconds)')
+plt.ylabel('Overlapping Allan deviation')
+plt.grid()
+popt, pcov = curve_fit(fit_function, t2,ad2)
+a2 = popt[0]
+b2 = popt[1]
+plt.loglog(t2, fit_function(t2, *popt), color='r', linestyle = 'dashed', label = f'fit:  a = {a2:.4f} \n b = {b2:.4f}')
+perr2 = np.sqrt(np.diag(pcov))
+plt.legend()
+plt.show()
 
 
-
-# t3, ad3, aderr3, adn3 = allantools.oadev(signal_locked, rate = 0.25, data_type="phase", taus = t)
-
-# fig = plt.figure()
-# plt.plot(np.log(t3), np.log(ad3), label = "locked laser")
-# plt.xlabel('Taus (seconds)')
-# plt.ylabel('Overlapping Allan deviation')
-# plt.grid()
-# popt, pcov = curve_fit(fit_function, np.log(t3), np.log(ad3))
-# a3 = popt[0]
-# b3 = popt[1]
-# plt.plot(np.log(t3), fit_function(np.log(t3), *popt), color='r', linestyle= 'dashed', label = f'linear fit: y = {a3:.3f}x + {b3:.3f}')
-# perr3 = np.sqrt(np.diag(pcov))
-# plt.legend()
-# plt.show()
+### Power-locked laser ### 
 
 
 
+t3, ad3, aderr3, adn3 = allantools.oadev(signal_locked, rate = 0.25, data_type="phase", taus = t)
 
-
-
-
-'''
-L'installation via pip de AllanTools s'est bien passée jusqu'à 
-WARNING: Ignoring invalid distribution -ip (c:\python39\lib\site-packages)
-  WARNING: Failed to write executable - trying to use .deleteme logic
-ERROR: Could not install packages due to an OSError: [WinError 2] Le fichier spécifié est introuvable: 'c:\\python39\\Scripts\\tabulate.exe' -> 'c:\\python39\\Scripts\\tabulate.exe.deleteme'
-
-Il faudra voir si cette erreur empêche le fonctionnement de AllanTools pour ce que je veux faire ou non 
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fig = plt.figure()
+plt.loglog(t3, ad3, label = "locked laser", color = 'b', linestyle = '', marker = '.')
+plt.xlabel('Taus (seconds)')
+plt.ylabel('Overlapping Allan deviation')
+plt.grid()
+popt, pcov = curve_fit(fit_function, t3, ad3)
+a3 = popt[0]
+b3 = popt[1]
+plt.loglog(t3, fit_function(t3, *popt), color='r', linestyle = 'dashed', label = f'fit:  a = {a3:.4f} \n b = {b3:.4f}')
+perr3 = np.sqrt(np.diag(pcov))
+plt.legend()
+plt.show()
